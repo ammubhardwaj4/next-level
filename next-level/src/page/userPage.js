669c7c011f00712ee40a610162b927102f613239
Amit Pathak <ammubhardwaj4@gmail.com>
2019-12-03 19:59:13 +0530

    Project inserted successful.

--- next-level/src/page/userPage.js
+++ next-level/src/page/userPage.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SideBar from "../components/Sidebar";
 import Profile from "../components/Profile";
 import "../css/style.css";
-
 
 export class userPage extends Component {
   render() {
@@ -14,16 +13,14 @@ export class userPage extends Component {
         <Header />
         <main className="main">
           <div className="user-view">
-            <SideBar />
-            <Profile />
+            <SideBar fromPage="user" />
+            <Profile fromPage="user" />
           </div>
         </main>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-
-export default withRouter(userPage)
-
+export default withRouter(userPage);
